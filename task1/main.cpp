e3019796236c983dde61f81b918f92ac3a0ff0b0
Luke D'Amato <82449473+lukedamato20@users.noreply.github.com>
2021-11-09 11:01:36 +0100

    created file and added struct + main

---
+++ task1/main.cpp
@@ -0,0 +1,20 @@
+/*CPS2004 - OOP
+  Task 1 - DAG (in C++)
+  Luke D'Amato
+*/
+
+#include <iostream>
+
+using namespace std;
+
+// creating a struct to hold the start and end of our DAG
+struct Edge
+{
+    int start, end;
+    // since we are working with DAGs, start != end
+};
+
+int main() 
+{
+
+}
