b980af204f586471022ed52fe35917bf7e4913cd
Luke D'Amato <luke.damato.20@um.edu.mt>
2021-11-09 11:31:48 +0100

    added object class for DAG, and added nodes adding (v1)

--- task1/main.cpp
+++ task1/main.cpp
@@ -4,15 +4,51 @@
 */
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 // creating a struct to hold the start and end of our DAG
-struct Edge
+struct edge
 {
     int start, end;
-    // since we are working with DAGs, start != end
+    // since we are working with DAGs, start != end (no cycles)
 };
+
+// representing the graph object in class DAG
+class DAG
+{
+    vector<vector<int>> graphList;
+
+    // creating the Graph constroctor to take a list of edges
+    DAG (vector<edge> const &edges, int n)
+    {
+        graphList.resize(n);
+
+        // adding edges to the DAG
+        for(auto &edge: edges)
+        {
+            // adding in a directed graph 
+            graphList[edge.start].push_back(edge.end);
+        }
+
+    }
+};
+
+// removing individual nodes
+// void removeNode()
+// {
+ 
+// }
+
+// returning the list of edges 
+// DO NOT DESTROY in-memorry struct
+// void outputDAG()
+// {
+
+// }
+
+
 
 int main() 
 {
