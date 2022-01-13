1d103501b42af38aa334e12034ad3fb4e3168778
Luke D'Amato <luke.damato.20@um.edu.mt>
2022-01-13 16:19:46 +0100

    added order concept
    v1

--- task2/OrderBook.java
+++ task2/OrderBook.java
@@ -1,4 +1,9 @@
 public class OrderBook
 {
+    // Orders can be BUY ORDERS, SELL ORDERS. 
+    // Each order can also can be a MARKET order or LIMIT order.
+    // Market orders do not have a bid or ask price
+    // 
 
+    // sorted data structure of orders
 }
