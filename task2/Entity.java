8d513f15a1c5138f823c9d686c4352572d698f7d
Luke D'Amato <luke.damato.20@um.edu.mt>
2022-01-21 11:38:42 +0100

    update entity.java
    added getters and setter + constructor

--- task2/Entity.java
+++ task2/Entity.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-
-public class Traders
+public class Entity
 {
     private String id;
     private String name;
@@ -8,10 +6,77 @@ public class Traders
     private String occupation;
     private String gender;
 
-    //getters ...
+    // getters and setters
+    public void setId(String id)
+    {
+        this.id = id;
+    }
 
-    //setters ...
+    public String getId()
+    {
+        return id;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setSurname(String surname)
+    {
+        this.surname = surname;
+    }
+
+    public String getSurname()
+    {
+        return surname;
+    }
+
+    public String getFullName()
+    {
+        return name + ", " + surname + ".";
+    }
+
+    public void setOccupation(String occupation)
+    {
+        this.occupation = occupation;
+    }
+
+    public String getOccupation()
+    {
+        return occupation;
+    }
+
+    public void setGender(String gender)
+    {
+        this.gender = gender;
+    }
+
+    public String getGender()
+    {
+        return gender;
+    }
 
     // initialise constructor ...
+    public Entity(String id, String name, String surname, String occupation, String gender)
+    {
+        setId(id);
+        setName(name);
+        setSurname(surname);
+        setOccupation(occupation);
+        setGender(gender);
+    }
 
+    public void viewEntityDetails()
+    {
+        System.out.println("Full Name :" + getFullName());
+        System.out.println("ID Number :" + getId());
+        System.out.println("Sex :" + getGender());
+        System.out.println("Occupation :" + getOccupation());
+    }
 }
