b8b496995402ec8ffd12410b2c772e25738321aa
Luke D'Amato <luke.damato.20@um.edu.mt>
2022-01-21 18:21:06 +0100

    fixed main method

--- task3/BigInts.cpp
+++ task3/BigInts.cpp
@@ -322,3 +322,23 @@ myuint<T> myuint<T>::operator%(myuint<T> num)
 
     return result;
 }
+
+int main()
+{
+    // testing code with num1 and num2 as big integer examples...
+    std::cout << "test output" << std::endl;
+
+    // creates a 1024-bit unsigned int ’5’
+    myuint<1024> num1(5);
+    std::cout << num1.getCurrent() << std::endl;
+
+    // shifts it by 1000 bits and adds 23
+    // ((( adding brackets to fix possible mistake?? )))
+    myuint<1024> num2 = (num1 << 1000) + 23; 
+    std::cout << num2.getCurrent() << std::endl;
+
+    // outputting result from convertion function
+    std::cout << num2.converType<int>() << std::endl;
+
+    return num2.template converType<int>(); // returns 23
+}
